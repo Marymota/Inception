fd06508c387c60d6575d0b93bb8ddc22d644f079
Marymota <dg.marciamota@gmail.com>
2023-07-27 20:20:43 +0100

    nginx error

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -10,7 +10,6 @@ cat << EOF > /var/www/wp-config.php
 # Database for WordPress name
 define( 'DB_NAME', '${DB_NAME}' );
 
-# MySQL database username
 define( 'DB_USER', '${DB_USER}' );
 
 # MySQL database password
@@ -24,7 +23,6 @@ define( 'DB_CHARSET', 'utf8' );
 
 # Database Collate type (sorting rules, case and sensitivity properties)
 define( 'DB_COLLATE', '' );
-
 
 # DEBUG MODE
 # Enable debug mode 
@@ -46,6 +44,71 @@ define('FS_METHOD','direct');
 # WP Database table prefix
 \$table_prefix = 'wp_';
 
+# Checks if wp-config.php file has already been created by a previous run of this script
+if [ -e wp-config.php ]; then
+	  echo "Wordpress config already created"
+else
+    # Create the wordpress config file
+    wp config create --allow-root \
+        --dbname=$db_name \
+        --dbuser=$db_user \
+        --dbpass=$db_pwd \
+        --dbhost=$db_host
+   chmod 600 wp-config.php
+fi
+
+# Check if wordpress is already installed
+if wp core is-installed --allow-root; then
+	  echo "Wordpress core already installed"
+else
+    # Installs wordpress
+    wp core install --allow-root \
+        --url=https://mmota.42.fr \
+        --title="Inception" \
+        --admin_user=$DB_USER \
+        --admin_email=mmota@42.fr \
+        --admin_password=$DB_PASS
+
+    wp user create --allow-root \
+        $DB_USER \
+        mmota@42.fr \
+        --role=author \
+        --user_pass=$DB_PASS
+
+    # Turns off debugging which is needed when using CLI from container
+    wp config set WORDPRESS_DEBUG false --allow-root
+fi
+
+# Check if author user has already been created by a previous run of this script
+if !(wp user list --field=user_login --allow-root | grep $DB_USER); then
+	# Create a new author user
+    wp user create --allow-root \
+        $DB_USER \
+        mmota@42.fr \
+        --role=author \
+        --user_pass=$DB_PASS
+fi
+
+wp user set-role $DB_USER administrator --allow-root
+
+wp plugin update --all --allow-root
+
+# Set comment settings
+
+wp option set comment_moderation 0 --allow-root
+wp option set moderation_notify 0 --allow-root
+wp option set comment_previously_approved 0 --allow-root
+wp option set close_comments_for_old_posts 0 --allow-root   
+wp option set close_comments_days_old 0 --allow-root
+
+# Sets the correct port to listen to nginx
+sed -ie 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/g' \
+/etc/php/7.4/fpm/pool.d/www.conf
+
+wp theme activate twentytwentytwo --allow-root
+
+chown -R wpg:wpg /var/www/html*
+
 # Absolute path to the WP directory 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
@@ -55,3 +118,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once ABSPATH . 'wp-settings.php';
 EOF
 fi
+
